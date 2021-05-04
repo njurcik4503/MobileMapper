3ce5be463b5d82c58f2e440a82f22fd065373d92
natalie <njurcik4503@stu.d214.org>
2021-05-04 15:26:43 -0500

    MobileMapper

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -9,30 +9,72 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var currentLocation:CLLocation!
     let locationManager = CLLocationManager()
     
+    var parks: [MKMapItem] = []
     
-    override func viewDidLoad() {
+     override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
         
         locationManager.delegate = self
+        mapView.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
     }
 
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
         currentLocation = locations[0]
     }
 
-
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        
+//        let center = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            guard let response = response else {return}
+            for currentMapItem in response.mapItems {
+                self.parks.append(currentMapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = currentMapItem.placemark.coordinate
+                annotation.title = currentMapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        return pin
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
 
 }
-
